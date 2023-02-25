c0674459f0318de569874146d1e872e67840a06e
Nataliya-grish <gray-wings@yandex.ru>
2023-02-25 15:19:18 +0300

    finalization ot the task

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,8 +1,11 @@
 import read from "./reader";
 import json from "./parser";
+import GameSaving from "./GameSaving";
 
 export default class GameSavingLoader {
   static load() {
-    return read().then((data) => json(data));
+    return read()
+      .then((data) => json(data))
+      .then((response) => new GameSaving(JSON.parse(response)));
   }
 }
